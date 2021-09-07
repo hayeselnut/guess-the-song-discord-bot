cd1f400b734296f9425f3ba40dec9ccb86e85754
Hayes Choy <hayeselnut2127@gmail.com>
2021-09-08 02:31:04 +1000

    DAT BIRD THO

--- product-page/src/App.tsx
+++ product-page/src/App.tsx
@@ -1,54 +1,89 @@
 import React from 'react';
-// import { Button } from 'react-bootstrap/Button';
-// import './App.css';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import InviteButton from './components/invite-button';
+import TopGGButton from './components/top-gg-button';
+
+import Bird from './assets/bird.svg';
 
 const App = () => {
   return (
     <>
       <header>
-        <h1>Guess the Song</h1>
-        <h2>Discord Bot</h2>
+        <Container style={{ border: '0px solid black' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',
+            flexWrap: 'wrap-reverse' }}
+          >
+            <div style={{ textAlign: 'right', border: '0px solid red' }}>
+              <h1>
+                <div className='gradientText'>Guess the Song</div>
+                <div>discord bot</div>
+              </h1>
+            </div>
+            <div style={{ width: '50%', border: '0px solid red' }}>
+              <Image src={Bird} fluid style={{ margin: '2rem' }} />
+            </div>
+          </div>
 
-        {/* <Button>Invite to your server</Button>
-        <Button>TOP.GG</Button> */}
-        <button>Invite to your server</button>
-        <button>TOP.GG</button>
+          <div style={{ marginTop: '10rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div>
+              <InviteButton />
+              <TopGGButton />
+            </div>
+          </div>
+        </Container>
       </header>
 
       <section id='how-to-start-with-spotify'>
-        <h2>Extracts songs from Spotify playlists and shuffles them into a game</h2>
-        <code>$start 10 link</code>
+        <Container>
+          <h2>Extracts songs from Spotify playlists and shuffles them into a game</h2>
+          <code>$start 10 link</code>
+        </Container>
       </section>
+
       <section id='guess-by-typing-in-a-text-channel'>
-        <h2>Easy guessing by typing into your text channel</h2>
-        <div style={{ backgroundColor: 'red', width: '600px', height: '200px' }} />
+        <Container>
+          <h2>Easy guessing by typing into your text channel</h2>
+          <div style={{ backgroundColor: 'red', width: '600px', height: '200px' }} />
+        </Container>
       </section>
+
       <section id='shuffle-multiple-playlists-into-one-game'>
-        <h2>Can combine multiple Spotify playlists together</h2>
-        <div style={{ backgroundColor: 'green', width: '600px', height: '200px' }} />
+        <Container>
+          <h2>Can combine multiple Spotify playlists together</h2>
+          <div style={{ backgroundColor: 'green', width: '600px', height: '200px' }} />
+        </Container>
       </section>
+
       <section id='customisable-configurations'>
-        <h2>Customisations</h2>
-        <div style={{ display: 'inline-block', backgroundColor: 'blue',
-          margin: '10px', width: '100px', height: '100px' }}
-        />
-        <div style={{ display: 'inline-block', backgroundColor: 'blue',
-          margin: '10px', width: '100px', height: '100px' }}
-        />
-        <div style={{ display: 'inline-block', backgroundColor: 'blue',
-          margin: '10px', width: '100px', height: '100px' }}
-        />
-        <div style={{ display: 'inline-block', backgroundColor: 'blue',
-          margin: '10px', width: '100px', height: '100px' }}
-        />
+        <Container>
+          <h2>Customisations</h2>
+          <div style={{ display: 'inline-block', backgroundColor: 'blue',
+            margin: '10px', width: '100px', height: '100px' }}
+          />
+          <div style={{ display: 'inline-block', backgroundColor: 'blue',
+            margin: '10px', width: '100px', height: '100px' }}
+          />
+          <div style={{ display: 'inline-block', backgroundColor: 'blue',
+            margin: '10px', width: '100px', height: '100px' }}
+          />
+          <div style={{ display: 'inline-block', backgroundColor: 'blue',
+            margin: '10px', width: '100px', height: '100px' }}
+          />
+        </Container>
       </section>
+
       <section id='ready-to-start-playing'>
-        <h2>Ready to start playing?</h2>
-        <button>Invite to your server</button>
+        <Container>
+          <h2>Ready to start playing?</h2>
+          <InviteButton />
+        </Container>
       </section>
-      <section id='contact'>
-        <span>Contact me</span>
-      </section>
+
+      <footer id='contact'>
+        <Container>
+          <span>Contact me</span>
+        </Container>
+      </footer>
     </>
   );
 };
