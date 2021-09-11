1aec7edade3f6623a4e36538a70048d982d28d05
Hayeselnut <hayeselnut2127@gmail.com>
2021-09-11 17:12:45 +1000

    build website

--- product-page/src/App.tsx
+++ product-page/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Image } from 'react-bootstrap';
+import { Container, Image, Tooltip } from 'react-bootstrap';
 import InviteButton from './components/invite-button';
 import TopGGButton from './components/top-gg-button';
 
@@ -42,7 +42,7 @@ const App = () => {
       <section id='spotify-integration'>
         <Container className='center debug'>
           <h2 className='feature-title'>Spotify integration</h2>
-          <p>Create a game with any Spotify playlist</p>
+          <p className='feature-caption'>Create a game with any Spotify playlist</p>
           <div className='start-command'>
             <code>
               $start 10 https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M
@@ -51,12 +51,12 @@ const App = () => {
         </Container>
       </section>
 
-      <section id='guessing'>
+      <section id='guessing' className='debug'>
         <Container className='feature'>
           <Image src={GuessingExample} className='guessing-example' width='40%' />
           <div style={{ width: '40%' }}>
             <h2 className='feature-title'>Guess by typing into your channel</h2>
-            <p>
+            <p className='feature-caption'>
               You get one point for the song name and every listed artist.
             </p>
           </div>
@@ -65,24 +65,24 @@ const App = () => {
         </div>
       </section>
 
-      <section id='customisations'>
+      <section id='customisations' className='debug'>
         <Container>
           <h2 className='feature-title'>Customisations</h2>
 
           <div className='customisations debug'>
-            <div className='customisation debug'>
+            <div className='customisation debug' id='round_duration'>
               <i className="bi bi-stopwatch" style={{ fontSize: '5rem' }} />
-              <caption className='debug'>Set time limit for each round</caption>
+              <caption className='feature-caption'>Turn up the pressure with faster rounds</caption>
             </div>
 
-            <div className='customisation debug'>
+            <div className='customisation debug' id='emote_nearly_correct_guesses'>
               <i className="bi bi-spellcheck" style={{ fontSize: '5rem' }} />
-              <caption className='debug'>Allow similar answers</caption>
+              <caption className='feature-caption'>Have the bot react to guesses that are a few characters off</caption>
             </div>
 
-            <div className='customisation debug'>
+            <div className='customisation debug' id='prefix'>
               <i className="bi bi-code" style={{ fontSize: '5rem' }} />
-              <caption className='debug'>Set command prefix for your server</caption>
+              <caption className='feature-caption'>Set command prefix for your server</caption>
             </div>
           </div>
 
